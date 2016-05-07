7d6892735d7fb0203832f442c04b5e7a27537b5d
Sergio Navarro <jersio@hotmail.com>
2016-05-07 14:15:04 +0200

    Added ContentToEdit token

--- src/Orchard.Web/Modules/Orchard.DynamicForms/Tokens/FormTokens.cs
+++ src/Orchard.Web/Modules/Orchard.DynamicForms/Tokens/FormTokens.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using Orchard.ContentManagement;
+using Orchard.DynamicForms.Elements;
 using Orchard.DynamicForms.Services.Models;
+using Orchard.Layouts.Framework.Elements;
 using Orchard.Tokens;
 
 namespace Orchard.DynamicForms.Tokens {
@@ -9,14 +12,43 @@ namespace Orchard.DynamicForms.Tokens {
             context.For("FormSubmission", T("Dynamic Form submission"), T("Dynamic Form Submission tokens for use in workflows handling the Dynamic Form Submitted event."))
                 .Token("Field:*", T("Field:<field name>"), T("The posted field value to access."), "Text")
                 .Token("IsValid:*", T("IsValid:<field name>"), T("The posted field validation status."))
-            ;
+                .Token("FormName", T("FormName"), T("The posted form's name."));
+            context.For("Element", T("Element"), T("Form Element tokens to get the content that is going to be edited through a dynamic form."))
+                .Token("ContentToEdit", T("ContentToEdit"), T("The content item to edit through a form."));            
         }
 
         public void Evaluate(EvaluateContext context) {
             context.For<FormSubmissionTokenContext>("FormSubmission")
                 .Token(token => token.StartsWith("Field:", StringComparison.OrdinalIgnoreCase) ? token.Substring("Field:".Length) : null, GetFieldValue)
                 .Chain(FilterChainParam, "Text", GetFieldValue)
-                .Token(token => token.StartsWith("IsValid:", StringComparison.OrdinalIgnoreCase) ? token.Substring("IsValid:".Length) : null, GetFieldValidationStatus);
+                .Token(token => token.StartsWith("IsValid:", StringComparison.OrdinalIgnoreCase) ? token.Substring("IsValid:".Length) : null, GetFieldValidationStatus)
+                .Token("FormName", GetFormName)
+                .Chain("FormName", "Text", GetFormName)
+                .Token("ContentToEdit", s => GetContentToEdit(s.Form))
+                .Chain("ContentToEdit", "Content", s => GetContentToEdit(s.Form));
+
+            context.For<Element>("Element")
+                .Token("ContentToEdit", GetContentToEdit)
+                .Chain("ContentToEdit", "Content", GetContentToEdit);
+        }
+
+        private object GetContentToEdit(Element element) {
+            Form form = element as Form;
+            if (form != null)
+                return form.ContentItemToEdit;
+            var formElement = element as FormElement;
+            if (formElement != null)
+                return formElement.Form.ContentItemToEdit;
+            while (element!=null) {
+                element = element.Container;
+                form = element as Form;
+                if (form != null)
+                    return form.ContentItemToEdit;
+                formElement = element as FormElement;
+                if (formElement != null)
+                    return formElement.Form.ContentItemToEdit;                
+            }
+            return null;
         }
 
         private static Tuple<string, string> FilterChainParam(string token) {
@@ -31,7 +63,10 @@ namespace Orchard.DynamicForms.Tokens {
         private string GetFieldValue(string fieldName, FormSubmissionTokenContext context) {
             return context.PostedValues[fieldName];
         }
-
+        private string GetFormName(FormSubmissionTokenContext context)
+        {
+            return context.Form.Name;
+        }
         private object GetFieldValidationStatus(string fieldName, FormSubmissionTokenContext context) {
             return context.ModelState.IsValidField(fieldName);
         }
